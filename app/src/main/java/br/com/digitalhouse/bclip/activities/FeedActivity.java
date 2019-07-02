d3b25a47394257dbd812d31b8aa63d9ab1ebe2d3
marianaam <marianaamgo@gmail.com>
2019-07-02 11:41:33 -0300

    bottom navigation menu

--- app/src/main/java/br/com/digitalhouse/bclip/activities/FeedActivity.java
+++ app/src/main/java/br/com/digitalhouse/bclip/activities/FeedActivity.java
@@ -1,28 +1,47 @@
 package br.com.digitalhouse.bclip.activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import br.com.digitalhouse.bclip.R;
 
 public class FeedActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
+    private TextView feedTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-        Intent intent = getIntent();
+        feedTextView = findViewById(R.id.feed_text_view);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        Bundle bundle = intent.getExtras();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-        String usuario = bundle.getString("NOME");
+                if(menuItem.getItemId() == R.id.home){
+                    feedTextView.setText("Home");
+                } else if (menuItem.getItemId() == R.id.favoritos){
+                    feedTextView.setText("Favoritos");
+                } else {
+                    feedTextView.setText("Busca");
+                }
 
-        TextView usuarioText = findViewById(R.id.feed_text_view);
+                return true;
+            }
+        });
 
-        usuarioText.setText("Olá, " + usuario);
+
+
+
 
 
     }
