5525a355a7477ac2e0682b075048b26e93e1cfb8
Erik Antunes <erik.antunes@live.com>
2019-07-18 09:50:31 -0300

    hunn

--- app/src/main/java/br/com/digitalhouse/bclip/activities/FeedActivity.java
+++ app/src/main/java/br/com/digitalhouse/bclip/activities/FeedActivity.java
@@ -11,8 +11,11 @@ import android.widget.TextView;
 
 import br.com.digitalhouse.bclip.R;
 import br.com.digitalhouse.bclip.adapter.FeedAdapter;
+import retrofit2.Retrofit;
 
 public class FeedActivity extends AppCompatActivity {
+
+    private Retrofit retrofit;
 
     private Button relevanciaButton;
     private Button recentesButton;
@@ -25,29 +28,24 @@ public class FeedActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-
-
         relevanciaButton = findViewById(R.id.relevancia_button);
         recentesButton = findViewById(R.id.recentes_button);
 
-
-
-
         recyclerView =findViewById(R.id.recycler_view);
-
-
         // configurando adapter
 
         FeedAdapter feedAdapter = new FeedAdapter();
-
-
-
         // configurando recyclerview
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(  feedAdapter  );
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://newsapi.org/v2/everything?domains=wsj.com&apiKey=ac329ede06bc4196bb9e52a18fd939c5")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
 
     }
